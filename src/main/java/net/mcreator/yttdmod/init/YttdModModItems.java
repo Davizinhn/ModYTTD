0b4721648fed6676c8f0ecf8069babdf8fe2c911
ANaanyy <lojonessantos@gmail.com>
2023-01-05 20:55:31 -0300

    cabecarizo

--- src/main/java/net/mcreator/yttdmod/init/YttdModModItems.java
+++ src/main/java/net/mcreator/yttdmod/init/YttdModModItems.java
@@ -21,6 +21,7 @@ public class YttdModModItems {
 	public static final RegistryObject<Item> CAIXA = block(YttdModModBlocks.CAIXA, CreativeModeTab.TAB_DECORATIONS);
 	public static final RegistryObject<Item> CODIGO = block(YttdModModBlocks.CODIGO, CreativeModeTab.TAB_REDSTONE);
 	public static final RegistryObject<Item> CHAVE = REGISTRY.register("chave", () -> new ChaveItem());
+	public static final RegistryObject<Item> CABECARIZO = block(YttdModModBlocks.CABECARIZO, CreativeModeTab.TAB_DECORATIONS);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
