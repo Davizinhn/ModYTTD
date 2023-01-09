22979d1575b6e5691fdfaf44237006709870b9de
Davizinhn <lojonessantos@gmail.com>
2023-01-08 22:55:45 -0300

    consegui importar o corpo da rizo. eu ouvi um amem?

--- src/main/java/net/mcreator/yttdmod/init/YttdModModBlocks.java
+++ src/main/java/net/mcreator/yttdmod/init/YttdModModBlocks.java
@@ -10,10 +10,17 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.level.block.Block;
 
+import net.mcreator.yttdmod.block.TorsoBlock;
+import net.mcreator.yttdmod.block.Teste3Block;
 import net.mcreator.yttdmod.block.PortaVidroBlock;
+import net.mcreator.yttdmod.block.Pernaesquerda2Block;
+import net.mcreator.yttdmod.block.Pernaesquerda1Block;
 import net.mcreator.yttdmod.block.CodigoBlock;
 import net.mcreator.yttdmod.block.CaixaBlock;
-import net.mcreator.yttdmod.block.CabecarizoBlock;
+import net.mcreator.yttdmod.block.CabecavamoversevaiBlock;
+import net.mcreator.yttdmod.block.BraconseiBlock;
+import net.mcreator.yttdmod.block.Bracoesquerdo1Block;
+import net.mcreator.yttdmod.block.Bracodireito2Block;
 import net.mcreator.yttdmod.YttdModMod;
 
 public class YttdModModBlocks {
@@ -21,5 +28,12 @@ public class YttdModModBlocks {
 	public static final RegistryObject<Block> CAIXA = REGISTRY.register("caixa", () -> new CaixaBlock());
 	public static final RegistryObject<Block> CODIGO = REGISTRY.register("codigo", () -> new CodigoBlock());
 	public static final RegistryObject<Block> PORTA_VIDRO = REGISTRY.register("porta_vidro", () -> new PortaVidroBlock());
-	public static final RegistryObject<Block> CABECARIZO = REGISTRY.register("cabecarizo", () -> new CabecarizoBlock());
+	public static final RegistryObject<Block> BRACOESQUERDO_1 = REGISTRY.register("bracoesquerdo_1", () -> new Bracoesquerdo1Block());
+	public static final RegistryObject<Block> PERNAESQUERDA_1 = REGISTRY.register("pernaesquerda_1", () -> new Pernaesquerda1Block());
+	public static final RegistryObject<Block> BRACODIREITO_2 = REGISTRY.register("bracodireito_2", () -> new Bracodireito2Block());
+	public static final RegistryObject<Block> PERNAESQUERDA_2 = REGISTRY.register("pernaesquerda_2", () -> new Pernaesquerda2Block());
+	public static final RegistryObject<Block> CABECAVAMOVERSEVAI = REGISTRY.register("cabecavamoversevai", () -> new CabecavamoversevaiBlock());
+	public static final RegistryObject<Block> TORSO = REGISTRY.register("torso", () -> new TorsoBlock());
+	public static final RegistryObject<Block> TESTE_3 = REGISTRY.register("teste_3", () -> new Teste3Block());
+	public static final RegistryObject<Block> BRACONSEI = REGISTRY.register("braconsei", () -> new BraconseiBlock());
 }
--- src/main/java/net/mcreator/yttdmod/init/YttdModModItems.java
+++ src/main/java/net/mcreator/yttdmod/init/YttdModModItems.java
@@ -31,13 +31,20 @@ public class YttdModModItems {
 	public static final RegistryObject<Item> CHAVE = REGISTRY.register("chave", () -> new ChaveItem());
 	public static final RegistryObject<Item> PORTA_VIDRO = doubleBlock(YttdModModBlocks.PORTA_VIDRO, CreativeModeTab.TAB_BUILDING_BLOCKS);
 	public static final RegistryObject<Item> DARDO = REGISTRY.register("dardo", () -> new DardoItem());
-	public static final RegistryObject<Item> CABECARIZO = block(YttdModModBlocks.CABECARIZO, CreativeModeTab.TAB_BUILDING_BLOCKS);
 	public static final RegistryObject<Item> CARTA = REGISTRY.register("carta", () -> new CartaItem());
 	public static final RegistryObject<Item> CARTA_SACRIFICIO = REGISTRY.register("carta_sacrificio", () -> new CartaSacrificioItem());
 	public static final RegistryObject<Item> CARTA_COMMONER = REGISTRY.register("carta_commoner", () -> new CartaCommonerItem());
 	public static final RegistryObject<Item> CARTA_CHAVEMESTRE = REGISTRY.register("carta_chavemestre", () -> new CartaChavemestreItem());
 	public static final RegistryObject<Item> CHAVE_2 = REGISTRY.register("chave_2", () -> new Chave2Item());
 	public static final RegistryObject<Item> LIXA = REGISTRY.register("lixa", () -> new LixaItem());
+	public static final RegistryObject<Item> BRACOESQUERDO_1 = block(YttdModModBlocks.BRACOESQUERDO_1, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> PERNAESQUERDA_1 = block(YttdModModBlocks.PERNAESQUERDA_1, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> BRACODIREITO_2 = block(YttdModModBlocks.BRACODIREITO_2, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> PERNAESQUERDA_2 = block(YttdModModBlocks.PERNAESQUERDA_2, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CABECAVAMOVERSEVAI = block(YttdModModBlocks.CABECAVAMOVERSEVAI, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> TORSO = block(YttdModModBlocks.TORSO, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> TESTE_3 = block(YttdModModBlocks.TESTE_3, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> BRACONSEI = block(YttdModModBlocks.BRACONSEI, CreativeModeTab.TAB_BUILDING_BLOCKS);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
